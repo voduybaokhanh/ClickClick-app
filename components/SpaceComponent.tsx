1b57c4110e41811d1960bda7e374535b94534ad9
private <thuvhmps22864@gmail.com>
2024-02-24 00:43:16 +0700

    SplashScreen

---
+++ components/SpaceComponent.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+interface Props {
+    width?: number;
+    height?: number;
+}
+const SpaceComponent =(props:Props)=>{
+    const{width,height}= props;
+    return(
+        <View
+        style={{
+            width,
+            height,
+        }}/>
+    )
+}
+export default SpaceComponent;
