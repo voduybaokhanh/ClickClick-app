25a1ad390e9f9eae9ee4dcc412ab0916a7837ea3
longhcpk02663 <104155372+longhcpk02663@users.noreply.github.com>
2024-03-06 21:40:56 +0700

    2

--- Screen/user/Notifications.js
+++ Screen/user/Notifications.js
@@ -8,57 +8,53 @@ import {
   ScrollView,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import React, { useState } from "react";
+import AxiosInstance from "./../../helper/Axiostance";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Dropdown } from "react-native-element-dropdown";
+import { StatusBar } from "expo-status-bar";
 
-const Notifications = ({}) => {
+const Notifications = () => {
+  const [isThatim, setIsThatim] = useState(false);
+  const handleBaocao = () => {
+    // Xử lý khi icon được ấn
+    console.log("Icon đã được ấn");
+    // Thêm mã xử lý bạn muốn thực hiện khi icon được ấn
+  };
+
+  const handleThatim = () => {
+    // Xử lý khi icon được ấn
+    console.log("Icon đã được ấn");
+    setIsThatim(!isThatim);
+    // Thêm mã xử lý bạn muốn thực hiện khi icon được ấn
+  };
+
   return (
-    <LinearGradient
-      locations={[0.05, 0.17, 0.8, 1]}
-      colors={["#3B21B7", "#8B64DA", "#D195EE", "#CECBD3"]}
-      style={styles.linearGradient}
-    >
-      <View style={styles.containers}>
-        <Text style={styles.Text15}> Forgot password?</Text>
-        <Text style={styles.Text16}>
-          Enter your email and we’ll send you the instructions on how to reset
-          your password.
-        </Text>
-
-        <View style={styles.container}>
-          <TextInput
-            placeholder="Password"
-            placeholderTextColor="#FFFFFF"
-            style={styles.TextInbutPassword}
-            value={password}
-            onChangeText={(text) => setPassword(text)}
+    <View style={styles.container}>
+      <LinearGradient
+        locations={[0.05, 0.17, 0.8, 1]}
+        colors={["#3B21B7", "#8B64DA", "#D195EE", "#CECBD3"]}
+        style={styles.linearGradient}
+      >
+        <TouchableOpacity>
+          <Image
+            style={styles.iconsetting}
+            source={require("../../Image/setting_icon.dpng")}
           />
-          {/* <Image style={styles.eye} source={require("../../Image/eye.png")} /> */}
-
-          <TextInput
-            placeholder="Confirm PassWord"
-            placeholderTextColor="#FFFFFF"
-            style={styles.TextInbutPassword}
-            value={password_confirm}
-            onChangeText={(text) => setpassword_confirm(text)}
-          />
-          <Text style={styles.Text11}>Enter code OTP</Text>
-          <OTPInput
-            style={styles.inputOTP}
-            numInputs={5}
-            value={otp}
-            onOtpChange={handleOtpChange}
-          />
-          {/* <Image style={styles.eye2} source={require("../../Image/eye.png")} /> */}
-
-          <TouchableOpacity
-            onPress={actionoRegister}
-            style={styles.buttonSignin}
-          >
-            <Text style={styles.Text3}>Recover PassWord</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </LinearGradient>
+        </TouchableOpacity>
+      </LinearGradient>
+    </View>
   );
 };
-
 export default Notifications;
+const styles = StyleSheet.create({
+  iconsetting: {},
+
+  linearGradient: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderRadius: 5,
+    flex: 1,
+    width: "100%",
+  },
+});
