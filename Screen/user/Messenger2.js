39d4073abe2609bbccd21e31c39460b0c9b07680
longhcpk02663 <104155372+longhcpk02663@users.noreply.github.com>
2024-03-09 14:44:10 +0700

    1

--- Screen/user/Messenger2.js
+++ Screen/user/Messenger2.js
@@ -1,24 +1,17 @@
 import React from "react";
 import { View, FlatList, Text } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
 
-const Notifications = () => {
+const Messenger2 = () => {
   // Tạo một mảng gồm 20 phần tử từ 0 đến 19
-  const data = Array.from(Array(5).keys());
+  const data = Array.from(Array(20).keys());
 
   // Hàm render mỗi dòng của danh sách
   const renderItem = ({ item }) => (
-    <LinearGradient
-      locations={[0.05, 0.17, 0.8, 1]}
-      colors={["#3B21B7", "#8B64DA", "#D195EE", "#CECBD3"]}
+    <View
       style={{ padding: 10, borderBottomWidth: 1, borderBottomColor: "#ccc" }}
     >
-      <View>
-        <Text>New </Text>
-      </View>
-
-      <Text style={{ color: "white" }}>{`Item ${item}`}</Text>
-    </LinearGradient>
+      <Text>{`Item ${item}`}</Text>
+    </View>
   );
 
   return (
@@ -30,4 +23,4 @@ const Notifications = () => {
   );
 };
 
-export default Notifications;
+export default Messenger2;
