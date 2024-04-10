6c5ab3390bc003d599202270a48ef2d9e7d19e64
Demo0501 <117192372+GKhiemm@users.noreply.github.com>
2024-04-10 23:12:43 +0700

    123

--- Screen/BottomTab/Home.js
+++ Screen/BottomTab/Home.js
@@ -206,6 +206,7 @@ const Home = () => {
       };
 
       const response = await instance.post("/likes-post.php", body);
+      console.log(response);
       // Cập nhật trạng thái isLikedMap
       const newIsLikedMap = { ...isLikedMap };
       newIsLikedMap[postid] = response.action === 1;
