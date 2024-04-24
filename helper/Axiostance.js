982d6b4c765475cbf707fb3893f569f7ce1e5bad
Tbin <Thanhbinhoke@gmail.com>
2024-04-24 16:13:53 +0700

    Update Axiostance.js

--- helper/Axiostance.js
+++ helper/Axiostance.js
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const AxiosInstance = async (contentType = 'application/json') => {
     const axiosInstance = axios.create({
-        baseURL: 'http://169.254.30.220:8686/'
+        baseURL: 'http://172.16.0.12:8686/'
     });
 
     axiosInstance.interceptors.request.use(
