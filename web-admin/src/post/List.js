4580dee30b4bf34b93478b5bc5a17bce1e7426fe
Tbin <Thanhbinhoke@gmail.com>
2024-04-25 13:53:18 +0700

    .

--- web-admin/src/post/List.js
+++ web-admin/src/post/List.js
@@ -6,22 +6,25 @@ const List = ({ saveUser }) => {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const axiosInstance = await AxiosInstance();
-                const response = await axiosInstance.get('/get-all-report.php');
-                if (response.status) {
-                    setPosts(response.posts);
-                } else {
-                    throw new Error(response.message);
-                }
-            } catch (error) {
-                console.error('Error fetching data:', error);
-                swal("Oops!", "Something went wrong while fetching data", "error");
-            }
-        }
         fetchData();
     }, []);
+
+    const fetchData = async () => {
+        try {
+            const axiosInstance = await AxiosInstance();
+            const response = await axiosInstance.get('/get-all-report.php');
+            if (response.status) {
+                // sắp xếp theo thời gian mới nhất
+                const sortedPosts = response.posts.sort((a, b) => new Date(b.TIME) - new Date(a.TIME));
+                setPosts(sortedPosts);
+            } else {
+                throw new Error(response.message);
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            swal("Oops!", "Something went wrong while fetching data", "error");
+        }
+    }
 
     const handleDelete = async (ID) => { // Thay đổi tham số từ ID thành postid
         swal({
@@ -39,7 +42,7 @@ const List = ({ saveUser }) => {
                     if (response.status) {
                         swal('Xóa thành công');
                         // Update the state after successful deletion
-                        setPosts(posts.filter(posts => posts.ID !== ID)); // Sử dụng postid thay vì ID
+                        setPosts(posts.filter(post => post.ID !== ID)); // Sử dụng postid thay vì ID
                     } else {
                         swal('Xóa thất bại');
                     }
@@ -52,7 +55,6 @@ const List = ({ saveUser }) => {
     }
 
     const handleCancelReport = async (ID) => {
-
         try {
             const axiosInstance = await AxiosInstance();
             const response = await axiosInstance.get(`/cancel.php?postid=${ID}`); // Thay đổi phương thức từ post sang get và truyền postid qua URL
@@ -60,7 +62,7 @@ const List = ({ saveUser }) => {
             if (response.status) {
                 swal('Hủy báo cáo thành công');
                 // Refresh list after successful cancellation
-                const updatedPosts = posts.filter(posts => posts.ID !== ID);
+                const updatedPosts = posts.filter(post => post.ID !== ID);
                 setPosts(updatedPosts);
             } else {
                 swal('Hủy báo cáo thất bại');
@@ -74,12 +76,14 @@ const List = ({ saveUser }) => {
         <div>
             <h1>Danh sách bài viết bị báo cáo</h1>
             <button className="btn btn-primary" onClick={() => saveUser(null)}>Đăng xuất</button>
+            <a href="/block" className="btn btn-warning">Chuyển đến trang quản lý người dùng</a>
+            <a href="/block2" className="btn btn-warning">Chuyển đến block2</a>
             <table className="table">
                 <thead>
                     <tr>
                         <th>ID</th>
-                        <th>Name</th>
-                        <th>Nội dung</th>
+                        <th>Tên</th>
+                        <th>Nội dung bài viết</th>
                         <th>Ảnh</th>
                         <th>Thời gian</th>
                         <th>Lý do tố cáo</th>
@@ -95,11 +99,10 @@ const List = ({ saveUser }) => {
                             <td style={{ maxWidth: "15px", maxHeight: "15px" }}>
                                 <img src={item.IMAGE} style={{ maxWidth: "100%", height: "auto" }} /> {/* Điều chỉnh kích thước hình ảnh */}
                             </td>
-
                             <td>{item.TIME}</td>
                             <td>{item.REASON}</td>
                             <td>
-                                <button className="btn btn-primary" onClick={() => handleCancelReport(item.ID)}>Cancel</button>
+                                <button className="btn btn-primary" onClick={() => handleCancelReport(item.ID)}>Hủy</button>
                                 <button className="btn btn-danger" onClick={() => handleDelete(item.ID)}>Xóa</button>
                             </td>
                         </tr>
